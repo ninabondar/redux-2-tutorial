4b7dfb5d260f14825df2ea609976f8af37ffc858
ninabondar <bondar_n@ucu.edu.ua>
2018-03-08 13:53:48 +0200

    Supplying the initial state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import todoApp from "./reducers/";
 import {createStore} from "redux";
+import todoApp from "./reducers";
 import {Provider} from "react-redux"
 import {App} from "./components/App"
 
+
+const persistedState = {
+    todos: [{
+        id: '0',
+        text: 'Welcome',
+        completed: false
+    }],
+    visibilityFilter: undefined
+};
+
+const store = createStore(
+    todoApp,
+    persistedState
+);
+console.log(store.getState());
+
 ReactDOM.render(
-    <Provider store={createStore(todoApp)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById("root")
