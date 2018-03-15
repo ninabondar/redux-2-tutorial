b1a581e15d6188046edea8c4d8077db8f2c0fcca
ninabondar <bondar_n@ucu.edu.ua>
2018-03-15 10:55:46 +0200

    collocating selectors w reducers

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,31 +2,15 @@ import {connect} from "react-redux";
 import TodoList from "./TodoList"
 import {toggleTodo} from "../actions/index";
 import { withRouter } from "react-router";
-
-const getVisibleTodos = (
-    todos,
-    filter
-) => {
-    switch (filter) {
-        case 'all':
-            return todos;
-        case 'completed':
-            return todos.filter(
-                t => t.completed
-            );
-        case 'active':
-            return todos.filter(
-                t => !t.completed
-            );
-    }
-};
+import { getVisibleTodos } from "../reducers";
 
 const mapStateToTodoListProps = (
     state,
     { match }
-) => ({
+) =>
+    ({
     todos: getVisibleTodos(
-        state.todos,
+        state,
         match.params.filter || "all"
     )
 });
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,31 +1,33 @@
 const todo = (state, action) => {
     switch (action.type) {
-        case "ADD_TODO":
+        case 'ADD_TODO':
             return {
                 id: action.id,
                 text: action.text,
-                completed: false
+                completed:false
             };
-        case "TOGGLE_TODO":
+        case 'TOGGLE_TODO':
             if (state.id !== action.id) {
                 return state;
             }
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 completed: !state.completed
-            });
+            };
         default:
             return state;
     }
-};
+
+}
 
 const todos = (state = [], action) => {
     switch (action.type) {
-        case "ADD_TODO":
+        case 'ADD_TODO':
             return [
                 ...state,
                 todo(undefined, action)
             ];
-        case "TOGGLE_TODO":
+        case 'TOGGLE_TODO':
             return state.map(t => todo(t, action));
         default:
             return state;
@@ -33,3 +35,22 @@ const todos = (state = [], action) => {
 };
 
 export default todos;
+
+
+export const getVisibleTodos = (
+    state,
+    filter
+) => {
+    switch (filter) {
+        case 'all':
+            return state;
+        case 'completed':
+            return state.filter(
+                t => t.completed
+            );
+        case 'active':
+            return state.filter(
+                t => !t.completed
+            );
+    }
+};
